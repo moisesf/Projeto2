511dcfba4d3abc5772b1fc5fd9e3ca95473c790a
moisesf <moisesf@gmail.com>
2018-02-17 08:32:45 -0300

    Novos arquivos.

--- app/src/main/java/concept/monitriip/fachada/Constantes.java
+++ app/src/main/java/concept/monitriip/fachada/Constantes.java
@@ -3,6 +3,6 @@ package concept.monitriip.fachada;
 public class Constantes {
 
     //public static final String ENDERECO_SERVIDOR = "http://192.168.0.36:8080/GPSCorporativo";
-    public static final String ENDERECO_SERVIDOR = "http://10.10.1.123:8080/GPSCorporativo";
-    //public static final String ENDERECO_SERVIDOR = "http://rastreamento.concept.inf.br/GPSCorporativo";
+    //public static final String ENDERECO_SERVIDOR = "http://10.10.1.123:8080/GPSCorporativo";
+    public static final String ENDERECO_SERVIDOR = "http://rastreamento.concept.inf.br/GPSCorporativo";
 }
